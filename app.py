752415a24acc8c1ae753f4360ff8c88a6c89a2f3
zeniroy <zeniroy@gmail.com>
2014-10-25 16:34:52 +0900

    remove useless and fix favicon error

--- app.py
+++ app.py
@@ -2,9 +2,6 @@
 
 from flask import Flask, render_template
 import requests
-import ast
-from collections import defaultdict
-from operator import attrgetter
 import json
 
 app = Flask(__name__)
@@ -15,10 +12,17 @@ from home import get_names
 def home():
     return render_template('home.html', id_names=get_names())
 
+@app.route("/favicon.ico")
+def favicon():
+	return "";
+
 @app.route("/<member_num>")
 def member(member_num):
 	f = open('19th members.json', 'r')
 	datas = json.load(f)
+	if len(datas) <= int(member_num):
+		return "wrong member id"
+
 	data = datas[int(member_num)]
 	data['yea'] = len([vote for vote in data['votes'] if vote['option'] == 'yea'])
 	data['nay'] = len([vote for vote in data['votes'] if vote['option'] == 'nay'])
